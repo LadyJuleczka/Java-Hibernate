afe2fa0a798895cd2322c9480c4893bee2e7bea6
student <julianna.mikucka@gmail.com>
2017-01-12 05:44:01 +0100

    utworzenie metod w ArticleManager

--- src/main/java/com/example/shdemo/service/ArticleManager.java
+++ src/main/java/com/example/shdemo/service/ArticleManager.java
@@ -12,8 +12,21 @@ import java.util.List;
 import com.example.shdemo.domain.Article;
 import com.example.shdemo.domain.UniqueAbility;
 
-public class ArticleManager {
+public interface ArticleManager {
+	
+	int addArticle(Article article);
+	boolean updateArticle(Article article);
+	void deleteArticle(Article article);
+	Article findArticleById(int id);
+	List<Article> getAllArticle();
+	List<Article> getRareArticles(boolean rare);
+	List<Article> getNotRareArticle();
+	
 	
 	void addUniqueAbility(UniqueAbility uniqueAbility);
-	
+	boolean updateUniqueAbility(UniqueAbility uniqueAbility);
+	void deleteUniqueAbility(UniqueAbility uniqueAbility);
+	UniqueAbility findUniqueAbilityByLevel(int level);
+	UniqueAbility findUniqueAbilityById(int id);
+	List<UniqueAbility> getAllUniqueAbility();
 }
